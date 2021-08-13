b3ec4053220ce44dca8ebd2bb8989f01cc202bf0
AlexOLary <AlexOLary@gmail.com>
2021-08-14 05:06:33 +0700

    Error 404

---
+++ pages/404.js
@@ -0,0 +1,15 @@
+export default function Error(){
+    return (
+        <h1>
+            <ul>
+                <li>В данный момет данная страница не доступна! </li>
+                <li>Мы знаем о проблеме и делаем все возможное,</li>
+                <li>что бы все заработало в скором времени!</li>
+                <li></li>
+                <li>This page is not available at the moment! </li>
+                <li>We are aware of the problem and are doing our best,< / li>
+                <li>to make everything work soon!</li>
+            </ul>
+        </h1>
+    )
+}
